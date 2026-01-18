9991e0715459b401e736252c24d1f47d70149892
loskyertt <loskyertt0403@gmail.com>
2026-01-19 01:21:47 +0800

    14th commit: tmp

---
+++ nixos_server/modules/locale_zh.nix
@@ -0,0 +1,22 @@
+# /etc/nixos/modules/locale_zh.nix
+{ config, pkgs, lib, ... }:
+
+{
+  # 设置系统的中文环境
+  i18n = {
+    defaultLocale = "zh_CN.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "zh_CN.UTF-8";
+      LC_IDENTIFICATION = "zh_CN.UTF-8";
+      LC_MEASUREMENT = "zh_CN.UTF-8";
+      LC_MONETARY = "zh_CN.UTF-8";
+      LC_NAME = "zh_CN.UTF-8";
+      LC_NUMERIC = "zh_CN.UTF-8";
+      LC_PAPER = "zh_CN.UTF-8";
+      LC_TELEPHONE = "zh_CN.UTF-8";
+      LC_TIME = "zh_CN.UTF-8";
+    };
+
+    supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+  };
+}
