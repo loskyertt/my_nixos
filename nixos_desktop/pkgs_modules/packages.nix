3eafebc9789d3154f89995590e7abe4a54b7037d
loskyertt <loskyertt0403@gmail.com>
2026-01-27 17:44:18 +0800

    tmp commit

--- nixos_desktop/pkgs_modules/packages.nix
+++ nixos_desktop/pkgs_modules/packages.nix
@@ -47,4 +47,19 @@
 
   # 设置默认 Shell (用户名：sky)
   users.users.sky.shell = pkgs.zsh;
+
+  # ---- 5. flatpak 配置 ----
+  # services.flatpak.enable = true;   # 启用 flatpak
+  # systemd.services.flatpak-repo = {  # 设置系统级软件的仓库
+  #   wantedBy = [ "multi-user.target" ];
+  #   path = [ pkgs.flatpak ];
+  #   script = ''
+  #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  #   '';
+  # };
+  # 让 GNOME 认得 Flatpak 的图标
+  # environment.extraInit = ''
+  #   [[ -d ~/.local/share/flatpak/exports/share ]] && export XDG_DATA_DIRS="$XDG_DATA_DIRS:$HOME/.local/share/flatpak/exports/share"
+  #   export XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/flatpak/exports/share"
+  # '';
 }
